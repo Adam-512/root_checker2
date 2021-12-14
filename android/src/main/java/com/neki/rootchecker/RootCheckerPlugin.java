0646e0cad78844c7e5409860c4fd770bf70fef1d
chenhui <512006634@qq.com>
2021-12-14 14:55:09 +0800

    upgrade to v2 embedding

--- android/src/main/java/com/neki/rootchecker/RootCheckerPlugin.java
+++ android/src/main/java/com/neki/rootchecker/RootCheckerPlugin.java
@@ -1,6 +1,5 @@
 package com.neki.rootchecker;
 
-
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.util.Log;
@@ -15,15 +14,31 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+// import io.flutter.plugin.common.MethodCall;
+// import io.flutter.plugin.common.MethodChannel;
+// import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+// import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+import com.neki.rootchecker.ConstantCollections;
+
+import android.app.Activity;
+import android.content.Context;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
-import com.neki.rootchecker.ConstantCollections;
 
 /** RootCheckerPlugin */
-public class RootCheckerPlugin implements MethodCallHandler {
+public class RootCheckerPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+  private MethodChannel channel;
+  private Context context;
+  private Activity activity;
+
   /** Plugin registration. */
   static Registrar vRegistrar;
 
@@ -31,6 +46,40 @@ public class RootCheckerPlugin implements MethodCallHandler {
     vRegistrar = registrar;
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "root_checker");
     channel.setMethodCallHandler(new RootCheckerPlugin());
+  }
+
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "root_checker");
+    channel.setMethodCallHandler(this);
+    context = flutterPluginBinding.getApplicationContext();
+  }
+
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding binding ) {
+    activity = binding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    // TODO: the Activity your plugin was attached to was
+    // destroyed to change configuration.
+    // This call will be followed by onReattachedToActivityForConfigChanges().
+  }
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
+    // TODO: your plugin is now attached to a new Activity
+    // after a configuration change.
+  }
+  @Override
+  public void onDetachedFromActivity() {
+    // TODO: your plugin is no longer associated with an Activity.
+    // Clean up references.
   }
 
   @Override
@@ -42,90 +91,88 @@ public class RootCheckerPlugin implements MethodCallHandler {
     }
   }
 
-  private void checkHpBodong(Result result){
+  private void checkHpBodong(Result result) {
     result.success(
-      isPathExist("su")
-      || isSUExist()
-      || isTestBuildKey()
-      || isHaveDangerousApps()
-      || isHaveRootManagementApps()
-      || isHaveDangerousProperties()
-      || isHaveReadWritePermission()
-    );
+        isPathExist("su")
+            || isSUExist()
+            || isTestBuildKey()
+            || isHaveDangerousApps()
+            || isHaveRootManagementApps()
+            || isHaveDangerousProperties()
+            || isHaveReadWritePermission());
   }
 
-  private boolean isPathExist(String ext){
-    for(String path : ConstantCollections.superUserPath){
+  private boolean isPathExist(String ext) {
+    for (String path : ConstantCollections.superUserPath) {
       String joinPath = path + ext;
       File file = new File(path, ext);
-      if(file.exists()){
-        Log.e("ROOT_CHECKER","Path is exist : "+joinPath);
+      if (file.exists()) {
+        Log.e("ROOT_CHECKER", "Path is exist : " + joinPath);
         return true;
       }
     }
     return false;
   }
 
-  private boolean isSUExist(){
+  private boolean isSUExist() {
     Process process = null;
-    try{
-      process = Runtime.getRuntime().exec(new String[]{"/system/xbin/which","su"});
+    try {
+      process = Runtime.getRuntime().exec(new String[] { "/system/xbin/which", "su" });
       BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-      if(in.readLine() != null){
-        Log.e("ROOT_CHECKER","cammand executed");
+      if (in.readLine() != null) {
+        Log.e("ROOT_CHECKER", "cammand executed");
         return true;
       }
       return false;
-    }catch (Exception e){
+    } catch (Exception e) {
       return false;
-    }finally {
-      if(process != null){
+    } finally {
+      if (process != null) {
         process.destroy();
       }
     }
   }
 
-  private boolean isTestBuildKey(){
+  private boolean isTestBuildKey() {
     String buildTags = Build.TAGS;
-    if(buildTags != null && buildTags.contains("test-keys")){
-      Log.e("ROOT_CHECKER","devices buid with test key");
+    if (buildTags != null && buildTags.contains("test-keys")) {
+      Log.e("ROOT_CHECKER", "devices buid with test key");
       return true;
     }
     return false;
   }
 
-  private boolean isHaveDangerousApps(){
+  private boolean isHaveDangerousApps() {
     ArrayList<String> packages = new ArrayList<String>();
     packages.addAll(Arrays.asList(ConstantCollections.dangerousListApps));
     return isAnyPackageFromListInstalled(packages);
   }
 
-  private boolean isHaveRootManagementApps(){
+  private boolean isHaveRootManagementApps() {
     ArrayList<String> packages = new ArrayList<>();
     packages.addAll(Arrays.asList(ConstantCollections.rootsAppPackage));
     return isAnyPackageFromListInstalled(packages);
   }
 
-
-  //check dangerous properties
-  private boolean isHaveDangerousProperties(){
-    final Map<String,String> dangerousProps = new HashMap<>();
+  // check dangerous properties
+  private boolean isHaveDangerousProperties() {
+    final Map<String, String> dangerousProps = new HashMap<>();
     dangerousProps.put("ro.debuggable", "1");
-    dangerousProps.put("ro.secure","0");
+    dangerousProps.put("ro.secure", "0");
 
     boolean result = false;
     String[] lines = commander("getprop");
-    if(lines == null){
+    if (lines == null) {
       return false;
     }
-    for(String line : lines){
-      for(String key : dangerousProps.keySet()){
-        if(line.contains(key)){
+    for (String line : lines) {
+      for (String key : dangerousProps.keySet()) {
+        if (line.contains(key)) {
           String badValue = dangerousProps.get(key);
-          badValue = "["+badValue+"]";
-          if(line.contains(badValue)){
-            Log.e("ROOT_CHECKER","Dangerous Properties with key : "+key +" and bad value : "+badValue);
-            result =  true;
+          badValue = "[" + badValue + "]";
+          if (line.contains(badValue)) {
+            Log.e("ROOT_CHECKER", "Dangerous Properties with key : " + key + " and bad value : " + badValue);
+            result = true;
           }
         }
       }
@@ -133,24 +180,24 @@ public class RootCheckerPlugin implements MethodCallHandler {
     return result;
   }
 
-  //canChangePermission
-  private boolean isHaveReadWritePermission(){
+  // canChangePermission
+  private boolean isHaveReadWritePermission() {
     Boolean result = false;
     String[] lines = commander("mount");
 
-    for(String line : lines){
+    for (String line : lines) {
       String[] args = line.split(" ");
-      if(args.length < 4){
+      if (args.length < 4) {
         continue;
       }
       String mountPoint = args[1];
       String mountOptions = args[3];
 
-      for(String path : ConstantCollections.notWritablePath){
-        if(mountPoint.equalsIgnoreCase(path)){
-          for(String opt : mountOptions.split(",")){
-            if(opt.equalsIgnoreCase("rw")){
-              Log.e("ROOT_CHECKER","Path : "+path+" is mounted with read write permission"+ line);
+      for (String path : ConstantCollections.notWritablePath) {
+        if (mountPoint.equalsIgnoreCase(path)) {
+          for (String opt : mountOptions.split(",")) {
+            if (opt.equalsIgnoreCase("rw")) {
+              Log.e("ROOT_CHECKER", "Path : " + path + " is mounted with read write permission" + line);
               result = true;
               break;
             }
@@ -162,28 +209,28 @@ public class RootCheckerPlugin implements MethodCallHandler {
     return result;
   }
 
-  private String[] commander(String command){
-    try{
+  private String[] commander(String command) {
+    try {
       InputStream inputStream = Runtime.getRuntime().exec(command).getInputStream();
-      if(inputStream == null){
-        return  null;
+      if (inputStream == null) {
+        return null;
       }
       String propVal = new Scanner(inputStream).useDelimiter("\\A").next();
       return propVal.split("\n");
-    }catch(Exception e ){
+    } catch (Exception e) {
       e.printStackTrace();
       return null;
     }
   }
 
-  private boolean isAnyPackageFromListInstalled(ArrayList<String> pkg){
+  private boolean isAnyPackageFromListInstalled(ArrayList<String> pkg) {
     boolean result = false;
-    PackageManager pm = vRegistrar.activeContext().getPackageManager();
-    for(String packageName : pkg){
-      try{
-        pm.getPackageInfo(packageName,0);
+    PackageManager pm = context.getPackageManager();
+    for (String packageName : pkg) {
+      try {
+        pm.getPackageInfo(packageName, 0);
         result = true;
-      }catch(Exception e){
+      } catch (Exception e) {
 
       }
     }
